9a493302a351b7be9b09c4317cee61e8f517135c
Aman Kumar Gupta <75511608+codecraft26@users.noreply.github.com>
2023-05-29 23:53:49 +0530

    Update Show.js

--- Models/Show.js
+++ Models/Show.js
@@ -1,9 +1,1 @@
-//schema for show
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
-var showSchema = new Schema({
-    movieName: {    
-        type: String,
-        required: true
-    }});
-module.exports = mongoose.model('Show', showSchema);
+// to do show model
